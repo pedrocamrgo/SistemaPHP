78853799fedd65b3dc735b6521b0440943d18b84
pedrocamrgo <nejjin@gmail.com>
2023-07-31 21:08:06 -0300

    pequenas alterações

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça seu Login </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+    <div class="form">
+    <img id="imguser" src="imagens/user.png" >
+    <form action="login.php" method="post">
+    <input type="text" name="username" placeholder="Usuário" id="username"><br>
+    <input type="password" name="password" placeholder="Senha" id="password"><br>
+    <input type="submit" value="Login" id="submit">
+    <link href="cadastro.php"> Cadastre-se </link>
+    </form>
+    </div>
+    
+    
+</body>
+</html>
